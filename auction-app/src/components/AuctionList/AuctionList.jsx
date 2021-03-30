8cad78e8cd38cafa697cd8a25e803a71a9afd662
OscarHgg <oscarhagglund@live.com>
2021-03-30 08:42:51 +0200

    AuctionList

--- auction-app/src/components/AuctionList/AuctionList.jsx
+++ auction-app/src/components/AuctionList/AuctionList.jsx
@@ -1,9 +1,11 @@
 import './AuctionList.css'
+import AuctionItem from '../AuctionItem/AuctionItem';
 
 const AuctionList = () => {
 
     let list = items.map(auctionItem => {
-        return(<AuctionItem item = {auctionItem}/>)
+        return(
+        <AuctionItem item ={auctionItem}/>)
     })
 
     return (<>{list}</>)
