b4c79899abcefc4729c3b2f2e84d30bda42fd83c
Jonathan Björnebro <jonathan.bjornebro@outlook.com>
2021-03-30 08:45:16 +0200

    Added graphical shell for PlaceBid

--- auction-app/src/containers/SelectedAuctionContainer.jsx
+++ auction-app/src/containers/SelectedAuctionContainer.jsx
@@ -1,5 +1,14 @@
-const SelectedAuctionContainer = () => {
-    return (<>
+import PlaceBid from '../components/PlaceBid/PlaceBid'
 
+
+const SelectedAuctionContainer = () => {
+
+    const CreateNewBid = () => {
+
+    }
+    return (<>
+        <PlaceBid create={CreateNewBid}></PlaceBid>
     </>)
 }
+
+export default SelectedAuctionContainer;
