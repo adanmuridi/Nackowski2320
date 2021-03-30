95659301614efabbe68a64787bf0f124948e70fc
OscarHgg <oscarhagglund@live.com>
2021-03-30 10:44:58 +0200

    SemantIc

--- auction-app/src/components/AuctionItem/AuctionItem.jsx
+++ auction-app/src/components/AuctionItem/AuctionItem.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import {Card} from 'semantic-ui-react';
 
 const AuctionItem = ({item}) => {
     return(
         <>
-        <div>
+        <Card>
+            <Card.Content>
+                <Card.Header>{item.Titel}</Card.Header>
+                <Card.Meta>{item.SlutDatum}</Card.Meta>
+            </Card.Content>
+        
         <h3>{item.Titel}</h3>
         <p>{item.Beskrivning}</p>
         <p>{item.SlutDatum}</p>
         <p>{item.Utropspris}</p>
         <p>{item.SkapadAv}</p>
-        </div>
+        
+        </Card>
+        
         </>
     );
 }
