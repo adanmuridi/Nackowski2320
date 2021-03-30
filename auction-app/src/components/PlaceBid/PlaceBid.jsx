2265e5579e7c7d9480d801c65a013e2636eb58c6
Jonathan Björnebro <jonathan.bjornebro@outlook.com>
2021-03-30 08:37:23 +0200

    Added shell for PlaceBid

---
+++ auction-app/src/components/PlaceBid/PlaceBid.jsx
@@ -0,0 +1,8 @@
+const PlaceBid = () => {
+
+    return (<>
+        <input placeholder="Bud"></input>
+        <input placeholder="Budgivare"></input>
+        <button>Placera bud</button>
+    </>)
+}
